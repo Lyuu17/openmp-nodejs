3152e06b6ad721bda2b8e3f8ef813d1a7e34450a
Lyuu17 <lyuu17@yandex.com>
2025-02-04 00:14:23 +0100

    update

--- openmp-nodejs/src/components/events/PlayerEventsComponent.cpp
+++ openmp-nodejs/src/components/events/PlayerEventsComponent.cpp
@@ -10,7 +10,14 @@ PlayerEventsComponent::PlayerEventsComponent(ICore* core)
     : m_core(core)
 {
     m_core->getPlayers().getPlayerConnectDispatcher().addEventHandler(this);
+    m_core->getPlayers().getPlayerStreamDispatcher().addEventHandler(this);
     m_core->getPlayers().getPlayerTextDispatcher().addEventHandler(this);
+    m_core->getPlayers().getPlayerShotDispatcher().addEventHandler(this);
+    m_core->getPlayers().getPlayerChangeDispatcher().addEventHandler(this);
+    m_core->getPlayers().getPlayerDamageDispatcher().addEventHandler(this);
+    m_core->getPlayers().getPlayerClickDispatcher().addEventHandler(this);
+    m_core->getPlayers().getPlayerCheckDispatcher().addEventHandler(this);
+    m_core->getPlayers().getPlayerUpdateDispatcher().addEventHandler(this);
 }
 
 PlayerEventsComponent::~PlayerEventsComponent()
@@ -18,7 +25,14 @@ PlayerEventsComponent::~PlayerEventsComponent()
     if (m_core)
     {
         m_core->getPlayers().getPlayerConnectDispatcher().removeEventHandler(this);
+        m_core->getPlayers().getPlayerStreamDispatcher().removeEventHandler(this);
         m_core->getPlayers().getPlayerTextDispatcher().removeEventHandler(this);
+        m_core->getPlayers().getPlayerShotDispatcher().removeEventHandler(this);
+        m_core->getPlayers().getPlayerChangeDispatcher().removeEventHandler(this);
+        m_core->getPlayers().getPlayerDamageDispatcher().removeEventHandler(this);
+        m_core->getPlayers().getPlayerClickDispatcher().removeEventHandler(this);
+        m_core->getPlayers().getPlayerCheckDispatcher().removeEventHandler(this);
+        m_core->getPlayers().getPlayerUpdateDispatcher().removeEventHandler(this);
     }
 }
 
@@ -90,7 +104,7 @@ bool PlayerEventsComponent::onPlayerText(IPlayer& player, StringView message)
         cancelled             = !v8cancelledValue.IsEmpty() && v8cancelledValue.ToLocalChecked()->BooleanValue(resource->m_isolate);
     });
 
-    return cancelled; // TODO
+    return cancelled;
 }
 
 bool PlayerEventsComponent::onPlayerCommandText(IPlayer& player, StringView message)
@@ -109,5 +123,112 @@ bool PlayerEventsComponent::onPlayerCommandText(IPlayer& player, StringView mess
         cancelled             = !v8cancelledValue.IsEmpty() && v8cancelledValue.ToLocalChecked()->BooleanValue(resource->m_isolate);
     });
 
-    return !cancelled; // TODO
+    return !cancelled;
 }
+
+bool PlayerEventsComponent::onPlayerRequestSpawn(IPlayer& player) { return true; }
+
+void PlayerEventsComponent::onPlayerSpawn(IPlayer& player)
+{
+    ResourceManager::Exec([&](Resource* resource) {
+        v8::Local<v8::Object> v8objPlayer = resource->ObjectFromExtension(queryExtension<PlayerComponent>(player));
+
+        resource->Emit("onPlayerSpawn", { v8objPlayer });
+    });
+}
+
+void PlayerEventsComponent::onPlayerStreamIn(IPlayer& player, IPlayer& forPlayer)
+{
+    ResourceManager::Exec([&](Resource* resource) {
+        v8::Local<v8::Object> v8objPlayer    = resource->ObjectFromExtension(queryExtension<PlayerComponent>(player));
+        v8::Local<v8::Object> v8objForPlayer = resource->ObjectFromExtension(queryExtension<PlayerComponent>(forPlayer));
+
+        resource->Emit("onPlayerStreamIn", { v8objPlayer, v8objForPlayer });
+    });
+}
+
+void PlayerEventsComponent::onPlayerStreamOut(IPlayer& player, IPlayer& forPlayer)
+{
+    ResourceManager::Exec([&](Resource* resource) {
+        v8::Local<v8::Object> v8objPlayer    = resource->ObjectFromExtension(queryExtension<PlayerComponent>(player));
+        v8::Local<v8::Object> v8objForPlayer = resource->ObjectFromExtension(queryExtension<PlayerComponent>(forPlayer));
+
+        resource->Emit("onPlayerStreamOut", { v8objPlayer, v8objForPlayer });
+    });
+}
+
+bool PlayerEventsComponent::onPlayerShotMissed(IPlayer& player, const PlayerBulletData& bulletData) { return true; }
+bool PlayerEventsComponent::onPlayerShotPlayer(IPlayer& player, IPlayer& target, const PlayerBulletData& bulletData) { return true; }
+bool PlayerEventsComponent::onPlayerShotVehicle(IPlayer& player, IVehicle& target, const PlayerBulletData& bulletData) { return true; }
+bool PlayerEventsComponent::onPlayerShotObject(IPlayer& player, IObject& target, const PlayerBulletData& bulletData) { return true; }
+bool PlayerEventsComponent::onPlayerShotPlayerObject(IPlayer& player, IPlayerObject& target, const PlayerBulletData& bulletData) { return true; }
+
+void PlayerEventsComponent::onPlayerScoreChange(IPlayer& player, int score)
+{
+    ResourceManager::Exec([&](Resource* resource) {
+        v8::Local<v8::Object> v8objPlayer = resource->ObjectFromExtension(queryExtension<PlayerComponent>(player));
+        v8::Local<v8::Number> v8score     = v8::Number::New(resource->m_isolate, score);
+
+        resource->Emit("onPlayerStreamOut", { v8objPlayer, v8score });
+    });
+}
+
+void PlayerEventsComponent::onPlayerNameChange(IPlayer& player, StringView oldName)
+{
+    ResourceManager::Exec([&](Resource* resource) {
+        v8::Local<v8::Object> v8objPlayer = resource->ObjectFromExtension(queryExtension<PlayerComponent>(player));
+
+        resource->Emit("onPlayerStreamOut", { v8objPlayer, Utils::v8Str(oldName.data()) });
+    });
+}
+
+void PlayerEventsComponent::onPlayerInteriorChange(IPlayer& player, unsigned newInterior, unsigned oldInterior)
+{
+    ResourceManager::Exec([&](Resource* resource) {
+        v8::Local<v8::Object> v8objPlayer   = resource->ObjectFromExtension(queryExtension<PlayerComponent>(player));
+        v8::Local<v8::Number> v8newInterior = v8::Number::New(resource->m_isolate, newInterior);
+        v8::Local<v8::Number> v8oldInterior = v8::Number::New(resource->m_isolate, oldInterior);
+
+        resource->Emit("onPlayerInteriorChange", { v8objPlayer, v8newInterior, v8oldInterior });
+    });
+}
+
+void PlayerEventsComponent::onPlayerStateChange(IPlayer& player, PlayerState newState, PlayerState oldState)
+{
+    ResourceManager::Exec([&](Resource* resource) {
+        v8::Local<v8::Object> v8objPlayer = resource->ObjectFromExtension(queryExtension<PlayerComponent>(player));
+        v8::Local<v8::Number> v8newState  = v8::Number::New(resource->m_isolate, newState);
+        v8::Local<v8::Number> v8oldState  = v8::Number::New(resource->m_isolate, oldState);
+
+        resource->Emit("onPlayerStateChange", { v8objPlayer, v8newState, v8oldState });
+    });
+}
+
+void PlayerEventsComponent::onPlayerKeyStateChange(IPlayer& player, uint32_t newKeys, uint32_t oldKeys)
+{
+    ResourceManager::Exec([&](Resource* resource) {
+        v8::Local<v8::Object> v8objPlayer = resource->ObjectFromExtension(queryExtension<PlayerComponent>(player));
+        v8::Local<v8::Number> v8newKeys   = v8::Number::New(resource->m_isolate, newKeys);
+        v8::Local<v8::Number> v8oldKeys   = v8::Number::New(resource->m_isolate, oldKeys);
+
+        resource->Emit("onPlayerKeyStateChange", { v8objPlayer, v8newKeys, v8oldKeys });
+    });
+}
+
+void PlayerEventsComponent::onPlayerDeath(IPlayer& player, IPlayer* killer, int reason)
+{
+    ResourceManager::Exec([&](Resource* resource) {
+        v8::Local<v8::Object> v8objPlayer = resource->ObjectFromExtension(queryExtension<PlayerComponent>(player));
+        v8::Local<v8::Object> v8objKiller = resource->ObjectFromExtension(queryExtension<PlayerComponent>(killer));
+        v8::Local<v8::Number> v8reason    = v8::Number::New(resource->m_isolate, reason);
+
+        resource->Emit("onPlayerKeyStateChange", { v8objPlayer, v8objKiller, v8reason });
+    });
+}
+
+void PlayerEventsComponent::onPlayerTakeDamage(IPlayer& player, IPlayer* from, float amount, unsigned weapon, BodyPart part) {}
+void PlayerEventsComponent::onPlayerGiveDamage(IPlayer& player, IPlayer& to, float amount, unsigned weapon, BodyPart part) {}
+void PlayerEventsComponent::onPlayerClickMap(IPlayer& player, Vector3 pos) {}
+void PlayerEventsComponent::onPlayerClickPlayer(IPlayer& player, IPlayer& clicked, PlayerClickSource source) {}
+void PlayerEventsComponent::onClientCheckResponse(IPlayer& player, int actionType, int address, int results) {}
+bool PlayerEventsComponent::onPlayerUpdate(IPlayer& player, TimePoint now) { return true; }
